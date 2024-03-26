835c9baaab6b1f397d7f772bb7bd2c9a34e84cdf
Ella <90807615+Michaela1909@users.noreply.github.com>
2024-03-26 11:43:35 +0700

    commit

--- system/attendances/updateAttendanceFunction.php
+++ system/attendances/updateAttendanceFunction.php
@@ -24,32 +24,48 @@ function updateAttendanceController()
 {
     // Memeriksa apakah semua data yang diperlukan tersedia
     if (
-        isset($_POST["attendanceId"]) && isset($_POST["date"])
-        && isset($_POST["studentName"]) && isset($_POST["courseCode"])
-        && isset($_POST["courseName"]) && isset($_POST["status"])
+        isset($_POST["studentId"]) && isset($_POST["date"])
+        && isset($_POST["status"]) && isset($_POST["courseCode"])
     ) {
-        $attendanceId = $_POST["attendanceId"];
+        $studentId = $_POST["studentId"];
         $date = $_POST["date"];
-        $studentName = $_POST["studentName"];
-        $courseCode = $_POST["courseCode"];
-        $courseName = $_POST["courseName"];
         $status = $_POST["status"];
+        $courseCode = $_POST["courseCode"];
+
+        // Memeriksa apakah status memiliki nilai yang valid (0 atau 1)
+        if ($status !== "0" && $status !== "1") {
+            // Jika status tidak valid, tampilkan pesan error
+            $_SESSION["error"] = "Status tidak valid.";
+            header("location: historyAttendance.php");
+            exit;
+        }
 
         try {
             // Koneksi ke database
             $connection = getConnection();
 
-            // Query SQL untuk memperbarui data kehadiran
-            $stmt = $connection->prepare("UPDATE attendance SET Date = :date, StudentName = :studentName, CourseCode = :courseCode, CourseName = :courseName, Status = :status WHERE AttendanceId = :attendanceId");
-            $stmt->bindParam(':date', $date);
-            $stmt->bindParam(':studentName', $studentName);
-            $stmt->bindParam(':courseCode', $courseCode);
-            $stmt->bindParam(':courseName', $courseName);
+            // Query SQL untuk memperbarui data kehadiran hanya pada status
+            $stmt = $connection->prepare("
+            UPDATE 
+                attendances
+            INNER JOIN 
+                schedules ON attendances.ScheduleId = schedules.ScheduleId 
+            INNER JOIN 
+                courses ON schedules.CourseId = courses.CourseId 
+            SET 
+                attendances.Status = :status 
+            WHERE 
+                attendances.StudentId = :studentId  
+                AND DATE(schedules.DateTime) = :date
+                AND courses.Code = :courseCode 
+            ");
             $stmt->bindParam(':status', $status);
-            $stmt->bindParam(':attendanceId', $attendanceId);
+            $stmt->bindParam(':studentId', $studentId);
+            $stmt->bindParam(':date', $date);
+            $stmt->bindParam(':courseCode', $courseCode);
             $stmt->execute();
 
-            // Redirect ke halaman viewAttendance.php setelah pembaruan berhasil
+            // Redirect ke halaman historyAttendance.php setelah pembaruan berhasil
             header("location: historyAttendance.php");
             exit;
         } catch (Exception $e) {
@@ -65,6 +81,9 @@ function updateAttendanceController()
         exit;
     }
 }
+
+
+
 
 function getAttendanceByStudentIdAndDateAndCourseCode($studentId, $date, $courseCode)
 {
@@ -82,7 +101,8 @@ function getAttendanceByStudentIdAndDateAndCourseCode($studentId, $date, $course
             CONCAT(buildings.Letter, classrooms.Code) AS Room, 
             DATE_FORMAT(schedules.DateTime, '%H:%i:%s') AS DateTime, 
             DATE_FORMAT(attendances.FingerprintTimeIn, '%H:%i:%s') AS TimeIn,
-            attendances.StudentId 
+            attendances.StudentId,
+            attendances.Status 
             FROM 
                 attendances 
             INNER JOIN 
