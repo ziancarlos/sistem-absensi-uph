a909b4fa43b17f2f56b564d16ba89a4a8a608648
Ella <90807615+Michaela1909@users.noreply.github.com>
2024-04-20 00:12:31 +0700

    Update dataStudent.php

--- system/students/dataStudent.php
+++ system/students/dataStudent.php
@@ -4,6 +4,9 @@ require_once ("dataStudentFunction.php");
 
 <?php require_once ("../components/header.php"); ?>
 
+<head>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.dataTables.min.css">
+</head>
 
 <body id="page-top">
 
@@ -166,8 +169,10 @@ require_once ("dataStudentFunction.php");
 
 
     <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
     <script>
     new DataTable('#example', {
+        responsive : true,
         columns: [{
                 data: 'id'
             }, {
