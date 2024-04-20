654782de0023ecd9b9f3c7fb1a63a0beff7a2995
Ella <90807615+Michaela1909@users.noreply.github.com>
2024-04-21 00:13:06 +0700

    Update addClassroom.php

--- system/classroom/addClassroom.php
+++ system/classroom/addClassroom.php
@@ -28,7 +28,9 @@ if (isset ($_POST['add'])) {
 
 <?php require_once ("../components/header.php"); ?>
 
-
+<head>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.dataTables.min.css">
+</head>
 <body id="page-top">
 
     <!-- Page Wrapper -->
@@ -182,8 +184,10 @@ if (isset ($_POST['add'])) {
 
     <?php require_once ("../components/js.php"); ?>
     <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
     <script>
         new DataTable('#example', {
+            responsive : true,
             columns: [{
                 data: 'id'
             }, {
