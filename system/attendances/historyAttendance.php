2d9e0093431892d9c57aadc2405f037d1e19ee9f
Ella <90807615+Michaela1909@users.noreply.github.com>
2024-04-21 00:15:12 +0700

    Update historyAttendance.php

--- system/attendances/historyAttendance.php
+++ system/attendances/historyAttendance.php
@@ -3,7 +3,9 @@ require_once ("historyAttendanceFunction.php");
 ?>
 
 <?php require_once ("../components/header.php"); ?>
-
+<head>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.dataTables.min.css">
+</head>
 <body id="page-top">
 
     <!-- Page Wrapper -->
@@ -179,9 +181,11 @@ require_once ("historyAttendanceFunction.php");
     <?php require_once ("../components/js.php"); ?>
 
     <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
     <script>
         $(document).ready(function () {
             var table = $('#example').DataTable({
+                responsive : true,
                 columns: [{
                     data: 'id'
                 },
